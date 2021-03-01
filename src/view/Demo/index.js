a9da723d686dc27f87635ac022a2a6ebf9fa4f92
jcWu <314645644@qq.com>
2021-03-01 15:06:26 +0800

    save

--- src/view/Demo/index.js
+++ src/view/Demo/index.js
@@ -18,40 +18,38 @@ class Demo extends React.Component {
   }
 
   componentDidMount() {
-    const { DemoStore } = this.props;
-    DemoStore.setToken(
-      "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNTk2NzU4MTE2MSIsImNyZWF0ZWQiOjE1NzA2ODkyOTI0NDYsImV4cCI6MTU3MTI5NDA5Mn0.gM3ycaLPPixkq4bu0SM_wLeCw8THpno3SkQQF8V_XToRJe8TP9Ff-sjHh8eb7Ar8bk9E10GV7TQRq-Fsgva2_Q"
-    );
+    // const { DemoStore } = this.props;
+    // DemoStore.setToken(
+    //   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNTk2NzU4MTE2MSIsImNyZWF0ZWQiOjE1NzA2ODkyOTI0NDYsImV4cCI6MTU3MTI5NDA5Mn0.gM3ycaLPPixkq4bu0SM_wLeCw8THpno3SkQQF8V_XToRJe8TP9Ff-sjHh8eb7Ar8bk9E10GV7TQRq-Fsgva2_Q"
+    // );
   }
 
   render() {
     const { DemoStore } = this.props;
     const { resData, pending } = DemoStore;
     return (
-      !pending && (
-        <>
-          <h1>mobile: {resData.mobile}</h1>
-          <ul>
-            <div className={styles.title}>子路由测试</div>
-            <Link to="/demo/child_one">跳转子路由1</Link>
-            <Link to="/demo/child_two">跳转子路由2</Link>
-            <Link to="/demo/child_three">跳转子路由3</Link>
-          </ul>
-          <Switch>
-            <Route path="/demo/child_one" component={ChildOne} />
-            <Route path="/demo/child_two" component={ChildTwo} />
-            <Route path="/demo/child_three" component={ChildThree} />
-          </Switch>
+      <>
+        <h1>mobile: {resData.mobile}</h1>
+        <ul>
+          <div className={styles.title}>子路由测试</div>
+          <Link to="/demo/child_one">跳转子路由1</Link>
+          <Link to="/demo/child_two">跳转子路由2</Link>
+          <Link to="/demo/child_three">跳转子路由3</Link>
+        </ul>
+        <Switch>
+          <Route path="/demo/child_one" component={ChildOne} />
+          <Route path="/demo/child_two" component={ChildTwo} />
+          <Route path="/demo/child_three" component={ChildThree} />
+        </Switch>
 
-          <ul>
-            <div className={styles.title}>mobx测试</div>
-            <Link to="/demo/mobx">mobx</Link>
-          </ul>
-          <Switch>
-            <Route path="/demo/mobx" component={MobxTest} />
-          </Switch>
-        </>
-      )
+        <ul>
+          <div className={styles.title}>mobx测试</div>
+          <Link to="/demo/mobx">mobx</Link>
+        </ul>
+        <Switch>
+          <Route path="/demo/mobx" component={MobxTest} />
+        </Switch>
+      </>
     );
   }
 }
